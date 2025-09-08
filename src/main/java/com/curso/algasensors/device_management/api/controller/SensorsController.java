0f8380c351f690963fcd5820fa7e9bfeaf62a75a
ejun1074 <luis@rsdata.inf.br>
2025-09-08 10:42:09 -0300

    Implementação das tarefas: update, delete,enable,disable

--- src/main/java/com/curso/algasensors/device_management/api/controller/SensorsController.java
+++ src/main/java/com/curso/algasensors/device_management/api/controller/SensorsController.java
@@ -20,18 +20,24 @@ import org.springframework.web.server.ResponseStatusException;
 @RequiredArgsConstructor
 public class SensorsController {
 
+    private static final String SENSOR_NOT_FOUND = "Sensor not found";
+    private static final String SENSOR_ALREADY_ENABLED = "Sensor already enabled";
+    private static final String SENSOR_ALREADY_DISABLED = "Sensor already disabled";
+    private static final String SENSOR_ID = "sensorId";
 
     private final SensorRepository repository;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public Page<SensorOutput> search(@PageableDefault Pageable pageable) {
         Page<Sensor> sensors = repository.findAll(pageable);
         return sensors.map(this::convertToOutput);
     }
     
     @GetMapping("{sensorId}")
+    @ResponseStatus(HttpStatus.OK)
     SensorOutput get(@PathVariable("sensorId") TSID sensorId) {
-        Sensor sensor = repository.findById(new SensorId(sensorId)).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Sensor not found"));
+        Sensor sensor = repository.findById(new SensorId(sensorId)).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, SENSOR_NOT_FOUND));
         return convertToOutput(sensor);
     }
 
@@ -60,5 +66,43 @@ public class SensorsController {
                 .protocol(sensor.getProtocol())
                 .enabled(sensor.getEnabled())
                 .build();
+    }
+    @PutMapping("{sensorId}")
+    @ResponseStatus(HttpStatus.OK)
+    SensorOutput update(@PathVariable("sensorId") TSID sensorId, @RequestBody SensorInput input) {
+        Sensor sensor = repository.findById(new SensorId(sensorId)).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, SENSOR_NOT_FOUND));
+        sensor.setName(input.getName());
+        sensor.setModel(input.getModel());
+        sensor.setLocation(input.getLocation());
+        sensor.setIp(input.getIp());
+        sensor.setProtocol(input.getProtocol());
+        sensor = repository.saveAndFlush(sensor);
+        return convertToOutput(sensor);
+    }
+
+    @PutMapping("{sensorId}/enable")
+    @ResponseStatus(HttpStatus.OK)
+    SensorOutput enable(@PathVariable("sensorId") TSID sensorId) {
+        Sensor sensor = repository.findById(new SensorId(sensorId)).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, SENSOR_NOT_FOUND));
+        if (Boolean.TRUE.equals(sensor.getEnabled())) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, SENSOR_ALREADY_ENABLED);
+        sensor.setEnabled(true);
+        sensor = repository.saveAndFlush(sensor);
+        return convertToOutput(sensor);
+    }
+
+    @DeleteMapping("{sensorId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void delete(@PathVariable("sensorId") TSID sensorId) {
+        Sensor sensor = repository.findById(new SensorId(sensorId)).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, SENSOR_NOT_FOUND));
+        repository.deleteById(sensor.getId());
+    }
+
+    @DeleteMapping("{sensorId}/enable")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void disable(@PathVariable("sensorId") TSID sensorId) {
+        Sensor sensor = repository.findById(new SensorId(sensorId)).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, SENSOR_NOT_FOUND));
+        if (Boolean.FALSE.equals(sensor.getEnabled())) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, SENSOR_ALREADY_DISABLED);
+        sensor.setEnabled(false);
+        repository.saveAndFlush(sensor);
     }
 }
